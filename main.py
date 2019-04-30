d223b59591034fda085cc75d520483656da7417e
Alexander Makarovsky <romylirem@gmail.com>
2019-04-30 14:29:19 +0300

    Create list from variables

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ a = open("file.txt", "r", encoding="utf-8")
 # soup = BeautifulSoup(driver.page_source, features="html.parser")
 soup = BeautifulSoup(a, features="html.parser")
 articles = soup.find_all("article")
+lis = []
 for article in articles:
     name = article.find("div", class_="item_footer").find("span", class_="item_name").text
     if name == "Любое количество золота":
         continue
-    price = article.find("span", class_="js-price").text
-    price_2 = article.find("span", class_="price_old").text
-    print(name, "-", "Со скидкой", price, ",", "Без скидки", price_2)
+    price_new = article.find("span", class_="js-price").text
+    price_old = article.find("span", class_="price_old").find("span", class_="js-price")
+    if price_old is None:
+        price_old = "-"
+    else:
+        price_old = price_old.text
+    dic = {"name": name, "price new": price_new, "price old": price_old}
+    lis.append(dic)
+print(lis)
+
